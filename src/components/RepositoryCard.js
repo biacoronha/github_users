a41aa4030ccd837408e49893830cef3da17e045c
Beatriz Coronha <biacoronha@gmail.com>
2021-11-03 12:40:33 -0300

    add link to repos

--- src/components/RepositoryCard.js
+++ src/components/RepositoryCard.js
@@ -3,9 +3,11 @@ import './RepositoryCard.css'
 export default function RepositoryCard(repository) {
     repository = repository.repository
     return (
-        <article>
-            <h2>{repository.name}</h2>
-            <span>{repository.description}</span>
-        </article>
+        <a href={repository.html_url}>
+            <article>
+                <h2>{repository.name}</h2>
+                <span>{repository.description}</span>
+            </article>
+        </a>  
     );
 }
